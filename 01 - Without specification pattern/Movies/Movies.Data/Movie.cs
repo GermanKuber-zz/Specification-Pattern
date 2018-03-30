c759ff2e9766cdd7e72e3b01ef1bdacf540b2a62
Germán Küber <german.kuber@outlook.com>
2018-03-30 12:42:00 -0300

    First commit

---
+++ 01 - Without specification pattern/Movies/Movies.Data/Movie.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Movies.Data
+{
+    public class Movie : Entity
+    {
+        public virtual string Name { get; }
+        public virtual DateTime ReleaseDate { get; }
+        public virtual MovieType Type { get; }
+        public virtual double Rating { get; }
+    }
+
+    public enum MovieType
+    {
+        Kids,
+        Teen,
+        Adult,
+        Xxx
+    }
+
+
+    public class MovieRepository
+    {
+
+        public IReadOnlyCollection<Movie> GetAll()
+        {
+            return new List<Movie>();
+        }
+    }
+}
